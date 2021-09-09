2f56decb40f701cbcf28e1cbecff6f126946eec6
Sierra <srlewis@unc.edu>
2021-09-08 22:04:41 -0400

    Progress on Exercise 1

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -1,6 +1,15 @@
 """Counting letters in a string."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730365963"
 
+letter_search: str = input("What letter do you want to search for? ")
+enter_word: str = input("Enter a word: ")
+total: int = 0 
+i: int = 0
 
-# Begin your solution here...
+while i < len(enter_word): 
+    if (enter_word[i]) == letter_search:
+        total = total + 1
+    i = i + 1
+
+print("Count: " + str(total))
--- exercises/ex02/repeat_beat.py
+++ exercises/ex02/repeat_beat.py
@@ -3,4 +3,20 @@
 __author__ = "730365963"
 
 
-# Begin your solution here...
+beat: str = input("What beat do you want to repeat? ")  # Begin your solution here...
+
+repeated_beat: int = int(input("How many times do you want to repeat it? "))
+
+i = 0
+beat_count = ""
+
+if repeated_beat <= 0: 
+    print("No beat...")
+else: 
+    while i < repeated_beat:
+        if i < repeated_beat - 1:
+            beat_count = beat_count + beat + " "
+        else: 
+            beat_count = beat_count + beat
+        i = i + 1
+    print(beat_count)
