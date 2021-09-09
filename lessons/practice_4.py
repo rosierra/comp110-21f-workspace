2a6e63742e45144bb989c58db8fd5e0eda5ad3f3
Sierra <srlewis@unc.edu>
2021-09-08 23:06:34 -0400

    Practice

---
+++ lessons/practice_4.py
@@ -0,0 +1,7 @@
+# The randint function is imported from the random library so that
+# you are able to generate integers at random.
+# 
+# Documentation: https://docs.python.org/3/library/random.html#random.randint
+#
+# For example, consider the function call expression: randint(1, 100)
+# It will evaluate to an int value >= 1 and <= 100. 
