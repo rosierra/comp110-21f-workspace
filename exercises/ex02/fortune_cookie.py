adbb3d969a2cc1c1a0d08a1237b0c66ba4321f2a
Sierra <srlewis@unc.edu>
2021-09-08 22:15:15 -0400

    Progress on Exercise 1

--- exercises/ex02/fortune_cookie.py
+++ exercises/ex02/fortune_cookie.py
@@ -2,15 +2,7 @@
 
 __author__ = "730365963"
 
-# The randint function is imported from the random library so that
-# you are able to generate integers at random.
-# 
-# Documentation: https://docs.python.org/3/library/random.html#random.randint
-#
-# For example, consider the function call expression: randint(1, 100)
-# It will evaluate to an int value >= 1 and <= 100. 
 from random import randint
-
 
 print("Your fortune cookies says...")
 fortune_cookie: int = int(randint(1, 4))
