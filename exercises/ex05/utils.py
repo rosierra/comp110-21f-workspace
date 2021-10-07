a3d56f01d3d1518acb31f96eda431027e1e53537
Sierra <srlewis@unc.edu>
2021-10-06 23:51:06 -0400

    Progress on Exercise 5

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,54 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730365963"
 
-# Define your functions below
+
+def only_evens(nums: list[int]) -> list[int]:
+    """Returns a list containing only the elements of the list that are even."""
+    i: int = 0 
+    even_list: list[int] = []
+    while i < len(nums):
+        if nums[i] % 2 == 0:
+            even_list.append(nums[i])
+        i += 1
+
+    return even_list
+
+
+print(only_evens([4, 4, 4]))
+
+
+def sub(a_list: list[int], x: int, y: int) -> list[int]:
+    """Generates a list which is a subset of the given list."""
+    ab: list[int] = []
+    empty_list: list[int] = []
+    if x < 0: 
+        x = 0
+    if y > len(a_list):
+        y = len(a_list) 
+    if len(a_list) == 0:
+        return empty_list 
+    if x > len(a_list):
+        return empty_list
+    if y <= 0: 
+        return empty_list
+    i: int = x
+    while i < y:
+        x = a_list[i]
+        ab.append(x)
+        i += 1
+    return ab
+
+
+a_list = [10, 20, 30, 40]
+print(sub(a_list, 1, 3))
+    
+
+def concat(first: list[int], second: list[int]) -> list[int]:
+    """Generates a list which contains all the elements of the first list followed by all the elements of the second list."""
+    combined_list: list[int] = []
+    combined_list = first + second 
+    return combined_list
+
+
+print(concat([1, 2, 3], [4, 5, 6]))
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,70 @@
 """Unit tests for list utility functions."""
 
-# TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730365963"
+
+
+from exercises.ex05.utils import only_evens, sub, concat
+
+
+def test_only_evens_four_items() -> None:
+    """Test the ability to return even numbers in a list of four items."""
+    nums: list[int] = [1, 2, 3, 4]
+    assert only_evens(nums) == [2, 4]
+
+
+def test_only_evens_all_odd() -> None:
+    """Test the result of a single item in an list."""
+    nums: list[int] = [1, 3, 5, 7]
+    assert only_evens(nums) == []
+
+
+def test_only_evens_empty() -> None:
+    """Test the result of an empty list."""
+    nums: list[int] = []
+    assert only_evens(nums) == []
+
+
+def test_sub_multiple() -> None: 
+    """Test the result of multiple integers for the subset list."""
+    a_list: list[int] = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+    x: int = 2
+    y: int = 8
+    assert sub(a_list, x, y) == [30, 40, 50, 60, 70, 80]
+
+
+def test_sub_random() -> None: 
+    """Test random integers in the subset list."""
+    a_list: list[int] = [3, 6, 4, 8, 27]
+    x: int = 1
+    y: int = 3
+    assert sub(a_list, x, y) == [6, 4]
+
+
+def test_sub_end_index() -> None: 
+    """Test an end index greater than length of the list."""
+    a_list: list[int] = [45, 67, 89, 20, 34]
+    x: int = 2
+    y: int = 6
+    assert sub(a_list, x, y) == [89, 20, 34]
+
+
+def test_concat_combine_equal() -> None:
+    """Test if lists of equal lengths concatenate."""
+    first: list[int] = [1, 2, 3]
+    second: list[int] = [4, 5, 6]
+    assert concat(first, second) == [1, 2, 3, 4, 5, 6]
+
+
+def test_concat_combine_different() -> None:
+    """Test concatenation of different length lists."""
+    first: list[int] = [2, 4, 6, 8]
+    second: list[int] = [10, 12]
+    assert concat(first, second) == [2, 4, 6, 8, 10, 12]
+
+
+def test_concat_empty() -> None: 
+    """Test the concatenation of empty lists."""
+    first: list[int] = []
+    second: list[int] = []
+    assert concat(first, second) == []
